07766395e76097f7527951a1072202c4a6c05e45
et16omn <83271518+et16omn@users.noreply.github.com>
2022-12-19 18:19:03 +0100

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -90,10 +90,11 @@ class HighwayEnv(AbstractEnv):
         rewards = self._rewards(action)
         reward = sum(self.config.get(name, 0) * reward for name, reward in rewards.items())
         if self.config["normalize_reward"]:
-            reward = utils.lmap(reward,
-                                [self.config["collision_reward"],
-                                 self.config["high_speed_reward"] + self.config["right_lane_reward"] + self.config["lane_change_reward"]],
-                                [0, 1])
+            reward = utils.lmap(reward, [self.config["collision_reward"], self.config["high_speed_reward"]], [0, 1])
+            #reward = utils.lmap(reward,
+            #                    [self.config["collision_reward"],
+            #                     self.config["high_speed_reward"] + self.config["right_lane_reward"] + self.config["lane_change_reward"]],
+            #                    [0, 1])
         reward *= rewards['on_road_reward']
         return reward
 
